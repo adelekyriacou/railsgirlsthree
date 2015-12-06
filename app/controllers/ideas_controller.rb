2bcf266f657ed9292151e8bcef41d5c72a05615c
Adele Kyriacou <adelekyriacou@gmail.com>
2015-12-06 20:21:22 +0000

    Fix whitespace on private functions

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -62,15 +62,15 @@ class IdeasController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_idea
-      @idea = Idea.find(params[:id])
-      @comments = @idea.comments.all
-      @comment = @idea.comments.build
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_idea
+    @idea = Idea.find(params[:id])
+    @comments = @idea.comments.all
+    @comment = @idea.comments.build
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def idea_params
-      params.require(:idea).permit(:name, :description, :picture)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def idea_params
+    params.require(:idea).permit(:name, :description, :picture)
+  end
 end
