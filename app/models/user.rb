a4fe8e3a55734e11be773f01c42b8779ae80b020
Adele Kyriacou <adelekyriacou@gmail.com>
2015-12-07 13:27:02 +0000

    Add Gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  include Gravtastic
+  is_gravtastic
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
